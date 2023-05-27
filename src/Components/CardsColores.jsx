a3a72fd4cd70b414a1e47679d2164033ca87cf35
Agustin978 <loboagus10@gmail.com>
2023-05-26 22:23:52 -0300

    Creado el componente para mostrar en cards el nombre de los colores y un recuadro del mismo.

---
+++ src/Components/CardsColores.jsx
@@ -0,0 +1,18 @@
+import { Card } from "react-bootstrap";
+
+const CardsColores = ({color, codigoColor}) => {
+    //console.log(color);
+    //console.log(codigoColor);
+    return (
+        <div>
+            <Card>
+                <Card.Title className="p-2 display-4 text-center">{color}</Card.Title>
+                <Card.Body className="text-center">
+                    <Card.Img style={{backgroundColor: codigoColor, width: '150px', height:'150px'}}></Card.Img>
+                </Card.Body>
+            </Card>
+        </div>
+    );
+};
+
+export default CardsColores;
